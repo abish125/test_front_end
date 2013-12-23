82ca1190b26286786f881c2dc36259c4ef305a99
abish125 <abish125@gmail.com>
2013-12-23 18:14:01 -0500

    my django app

--- mysite/settings.py
+++ mysite/settings.py
@@ -50,7 +50,7 @@ MEDIA_ROOT = ("/Users/andrewbishara/Desktop/mysite/mysite/resources/")
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/resources/'
+MEDIA_URL = '/mysite/resources/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -103,7 +103,7 @@ ROOT_URLCONF = 'mysite.urls'
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 TEMPLATE_DIRS = (
-    "/Users/andrewbishara/Desktop/mysite/mysite/templates/"
+    "/mysite/templates/"
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
