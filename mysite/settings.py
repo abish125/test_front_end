3fcf6c95fcc6d0ca67cffd39e64de4ffa2fa8169
abish125 <abish125@gmail.com>
2013-12-26 12:39:36 -0500

    my django app

--- mysite/settings.py
+++ mysite/settings.py
@@ -46,12 +46,12 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'resources'),
+MEDIA_ROOT = os.path.join(BASE_DIR, 'resources')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = os.path.join(BASE_DIR, 'resources/'),
+MEDIA_URL = '/resources/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
