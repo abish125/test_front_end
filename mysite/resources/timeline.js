cf009fa74895bc5407de1676fb74037c28a60b87
abish125 <abish125@gmail.com>
2013-12-26 15:01:10 -0500

    my django app

--- mysite/resources/timeline.js
+++ mysite/resources/timeline.js
@@ -1,5 +1,6 @@
 var container = document.getElementById('mytimeline');
 var data = JSON.parse({{myposts}});
+document.write(data);
 //[
 //{id: 1, content: 'item 1', start: '2013-04-20'},
 //{id: 2, content: 'item 2', start: '2013-04-14'},
