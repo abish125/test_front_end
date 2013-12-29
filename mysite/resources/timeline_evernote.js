752c49c24c0013e8f45f080aa0ba3fa002693f1d
glebk <gleb.kuznetsov@gmail.com>
2013-12-28 18:33:45 -0700

    Use Backbone with javascript.

--- mysite/resources/timeline_evernote.js
+++ mysite/resources/timeline_evernote.js
@@ -6,37 +6,50 @@
 var sd = sd || {};
 
 
-/**
- * Update the timeline with new data.
- *
- * For now, this just empties the div and gets new data from the server.
- * Eventually we'll want to do per day requests or something like that.
- */
-sd.updateTimelineData = function() {
-     // Remove the previous timeline object.
-    $('#timeline-hook').empty();
+/** Backbone view object to wrap the timeline. */
+sd.EvernoteTimelineView = Backbone.View.extend({
 
-    // Make a request for data from the server and redraw the timeline
-    // once received.
-    $.get('/polls/_/get_evernote_timeline_data/', function(data) {
-        // Convert the data into an object.
-        // JSON should be available on most modern browsers (at least Chrome).
-        var parsedData = JSON.parse(data);
+    initialize: function() {
 
-        // Create and render a new timeline object.
-        sd.timeline = new vis.Timeline(sd.containerEl, parsedData, sd.options);
+        // Element to which we attach the timeline.
+        this.timelineHookEl = $('#timeline-hook')[0];
 
-    });
-};
+        // Options to timeline widget.
+        this.options = {};
+
+        // Timeline object attached to this.
+        this.timeline = null;
+
+        // Load the initial data.
+        this.updateTimelineData();
+    },
+
+    /**
+     * Update the timeline with new data.
+     *
+     * For now, this just empties the div and gets new data from the server.
+     * Eventually we'll want to do per day requests or something like that.
+     */
+    updateTimelineData: function() {
+         // Remove the previous timeline object.
+        $('#timeline-hook').empty();
+
+        // Make a request for data from the server and redraw the timeline
+        // once received.
+        $.get('/polls/_/get_evernote_timeline_data/', _.bind(function(data) {
+            // Convert the data into an object.
+            var parsedData = JSON.parse(data);
+
+            // Create and render a new timeline object.
+            this.timeline = new vis.Timeline(this.timelineHookEl, parsedData,
+                    this.options);
+
+        }, this));
+    },
+});
 
 
 // JavaScript starts running here on page load complete.
 $(document).ready(function() {
-    // Put all the objects under the sd namespace.
-    // TODO: Use proper object patterns.
-    sd.containerEl = $('#timeline-hook')[0];
-    sd.options = {};
-
-    // Perform initial update.
-    sd.updateTimelineData();
+    var view = new sd.EvernoteTimelineView();
 });
