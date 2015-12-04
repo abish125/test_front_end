767499c7898e195130197e531fd0f6ca1dfdb2e8
abish125 <abish125@gmail.com>
2015-12-04 02:31:20 +0000

    try again

---
+++ mysite/resources/geometry.js
@@ -0,0 +1,14 @@
+{
+	"metadata": {
+		"version": 4.4,
+		"type": "Geometry",
+		"generator": "Geometry.toJSON"
+	},
+	"uuid": "2E0C36E3-3ED0-480B-818F-E6AAE2500DF5",
+	"type": "TorusGeometry",
+	"radius": 100,
+	"tube": 40,
+	"radialSegments": 8,
+	"tubularSegments": 6,
+	"arc": 6.283185307179586
+}
