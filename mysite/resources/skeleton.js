09b2108279d4ede0244b09d33d567628025ec4d1
abish125 <abish125@gmail.com>
2016-07-05 17:55:43 +0000

    adding skeleton

--- mysite/resources/skeleton.js
+++ mysite/resources/skeleton.js
@@ -1,9 +1,4 @@
 <html>
-  <head>
-    <title>cannon.js - spring demo</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
-  </head>
   <body>
     <script src="{{MEDIA_URL}}cannon.js"></script>
     <script src="{{MEDIA_URL}}cannon.demo.js"></script>
