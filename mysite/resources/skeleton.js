2d95c160164e48b557f30c8395f1789e68394150
abish125 <abish125@gmail.com>
2016-07-05 18:10:48 +0000

    adding skeleton

--- mysite/resources/skeleton.js
+++ mysite/resources/skeleton.js
@@ -1,56 +1,42 @@
-<html>
-  <body>
-    <script src="{{MEDIA_URL}}cannon.js"></script>
-    <script src="{{MEDIA_URL}}cannon.demo.js"></script>
-    <script src="{{MEDIA_URL}}dat.gui.js"></script>
-    <script src="{{MEDIA_URL}}Three.js"></script>
-    <script src="{{MEDIA_URL}}TrackballControls.js"></script>
-    <script src="{{MEDIA_URL}}Detector.js"></script>
-    <script src="{{MEDIA_URL}}stats.min.js"></script>
-    <script src="{{MEDIA_URL}}smoothie.js"></script>
-    <script type="text/javascript">
+var demo = new CANNON.Demo();
+var size = 1;
 
-    var demo = new CANNON.Demo();
-    var size = 1;
+demo.addScene("Box", function()
+{
 
-    demo.addScene("Box",function(){
+    // Create world 
+    var world = demo.getWorld();
+    world.gravity.set(0,0,-10);
+    world.broadphase = new CANNON.NaiveBroadphase();
 
-        // Create world var world = demo.getWorld();
-        world.gravity.set(0,0,-10);
-        world.broadphase = new CANNON.NaiveBroadphase();
+    // Create a static sphere
+    var sphereShape = new CANNON.Sphere(0.1),
+        sphereBody = new CANNON.Body({ mass: 0 });
+    sphereBody.addShape(sphereShape);
+    world.add(sphereBody);
+    demo.addVisual(sphereBody);
 
-        // Create a static sphere
-        var sphereShape = new CANNON.Sphere(0.1),
-            sphereBody = new CANNON.Body({ mass: 0 });
-        sphereBody.addShape(sphereShape);
-        world.add(sphereBody);
-        demo.addVisual(sphereBody);
+    // Create a box body
+    var halfExtents = new CANNON.Vec3(size,size*0.3,size),
+        boxShape = new CANNON.Box(halfExtents);
+        boxBody = new CANNON.Body({ mass: 5 });
+    boxBody.addShape(boxShape);
+    boxBody.position.set(-size,0,-size);
+    world.add(boxBody);
+    demo.addVisual(boxBody);
 
-        // Create a box body
-        var halfExtents = new CANNON.Vec3(size,size*0.3,size),
-            boxShape = new CANNON.Box(halfExtents);
-            boxBody = new CANNON.Body({ mass: 5 });
-        boxBody.addShape(boxShape);
-        boxBody.position.set(-size,0,-size);
-        world.add(boxBody);
-        demo.addVisual(boxBody);
-
-        var spring = new CANNON.Spring(boxBody,sphereBody,{
-            localAnchorA: new CANNON.Vec3(size,0,size),
-            localAnchorB: new CANNON.Vec3(0,0,0),
-            restLength : 0,
-            stiffness : 50,
-            damping : 1,
-        });
-
-        // Compute the force after each step
-        world.addEventListener("postStep",function(event){
-            spring.applyForce();
-        });
+    var spring = new CANNON.Spring(boxBody,sphereBody,{
+        localAnchorA: new CANNON.Vec3(size,0,size),
+        localAnchorB: new CANNON.Vec3(0,0,0),
+        restLength : 0,
+        stiffness : 50,
+        damping : 1,
     });
 
-    demo.start();
+    // Compute the force after each step
+    world.addEventListener("postStep",function(event){
+        spring.applyForce();
+    });
+});
 
-    </script>
-  </body>
-</html>
+demo.start();
